66aea7e3b3603f6984e25c525cdc3500ae4dee9e
BlackGameOn <67538261+BlackGameOn@users.noreply.github.com>
2021-12-26 22:10:55 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,11 +8,16 @@ class CpuTemperature(MycroftSkill):
 
     @intent_handler('temperature.cpu.intent')
     def handle_temperature_cpu(self, message):
-        python //home//pi//kerems//LED_AC.py
-        self.speak("Led is On") 
+        try:
+            python //home//pi//kerems//LED_AC.py
+            self.speak("Led is On") 
+        except:
+            self.speak("Led Operation İs Failed BlackGame")
 
 def create_skill():
     return CpuTemperature()
+
+
 
 
 
