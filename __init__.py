6551f731987cd6916e10d4bd801c6d68ecb2f908
BlackGameOn <67538261+BlackGameOn@users.noreply.github.com>
2021-12-26 20:40:22 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from mycroft import MycroftSkill, intent_handler
+
+
+class CpuTemperature(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+        
+
+    @intent_handler('temperature.cpu.intent')
+    def handle_temperature_cpu(self, message):
+        python //home//pi//kerems//LED_AC.py
+        self.speak_dialog('temperature.cpu.dialog') 
+
+def create_skill():
+    return CpuTemperature()
+
